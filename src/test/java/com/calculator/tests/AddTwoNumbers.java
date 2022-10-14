57eaecc4071235841a9cf96d7cae94ebbed99e4c
elyas <elyas.arkin@hotmail.se>
2022-10-14 23:41:44 +0200

    added smple test

--- src/test/java/com/calculator/tests/AddTwoNumbers.java
+++ src/test/java/com/calculator/tests/AddTwoNumbers.java
@@ -1,5 +1,7 @@
 package com.calculator.tests;
 
+import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class AddTwoNumbers extends TestBase{
@@ -7,6 +9,19 @@ public class AddTwoNumbers extends TestBase{
 
     @Test
     void addingTwoNumbers(){
+
+        By digit_01 = By.id("btn_1");
+        By digit_02 = By.id("btn_2");
+        By addOperation = By.id("plus");
+        By equalOperation = By.id("equal");
+        By formula = By.id("formula");
+        driver.findElement(digit_01).click();
+        driver.findElement(addOperation).click();
+        driver.findElement(digit_02).click();
+        driver.findElement(equalOperation).click();
+        String result = driver.findElement(formula).getText();
+        Assert.assertEquals(result, "4");
+        Assert.assertTrue(driver.findElement(By.id("result")).isDisplayed());
 
     }
 }
